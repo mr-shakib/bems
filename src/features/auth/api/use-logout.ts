364172995be81ef011c61a3d387639d7b467132f
Shakib Howlader <contactshakibhere@gmail.com>
2025-08-16 20:59:53 +0600

    feat: added workspace member and invite code

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -26,6 +26,7 @@ export const useLogout = () => {
       toast.success("Logout successful!");
       router.refresh();
       queryClient.invalidateQueries({ queryKey: ["current"] });
+      queryClient.invalidateQueries({ queryKey: ["workspaces"] });
     },
     onError: () => {
       toast.error("Logout failed!");
