dc4fa07767738611a52427bf6c49126d041a5c4c
Md Sabbir Ahamed <redoyahamedrs@gmail.com>
2025-09-06 01:40:51 +0600

    task api completed except taskSwitcher and Project type

--- src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -3,8 +3,8 @@ import { redirect } from "next/navigation";
 
 const WorkspaceIdPage = async () => {
   const user = await getCurrent();
-  
-  if(!user) redirect("/sign-in");
+
+  if (!user) redirect("/sign-in");
   return (
     <div>
       <h1>Workspace ID Page</h1>
